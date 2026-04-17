74717243d06ade9ad15621c5254a853a13b572ce
rsilva9 <ramasilva909@gmail.com>
2026-04-17 19:08:04 -0300

    feat: Endpoint de prueba de servicio de donaciones

---
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/controllers/HolaController.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frba.ddsi.donaciones.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+    @GetMapping("/hola")
+    public String hola(){
+        return "Hola Sistema de Donaciones!";
+    }
+}

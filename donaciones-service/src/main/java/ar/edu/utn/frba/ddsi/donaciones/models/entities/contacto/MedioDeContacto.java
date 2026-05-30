99b0106b694d20e199a0879eb64c7fbaea0459a1
LaSudestada <166956850+LaSudestada@users.noreply.github.com>
2026-05-30 19:00:41 -0300

    Eliminamos las clases DonanteHumano y DonanteJuridico para unirlas en una sola clase donante y corregimos las clases asociadas, Genero, Rubro, Tipo de Documento, Tipo de Persona, aplicamos la preferencia en el Medio de Contacto

--- donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/contacto/MedioDeContacto.java
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/contacto/MedioDeContacto.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 public class MedioDeContacto {
     private TipoMedioContacto tipoMedioContacto;
     private String direccion;
+    private Boolean esPredeterminado;
 
-    public MedioDeContacto(TipoMedioContacto tipoMedioContacto, String direccion) {
+    public MedioDeContacto(TipoMedioContacto tipoMedioContacto, String direccion, Boolean esPredeterminado) {
         this.tipoMedioContacto = tipoMedioContacto;
         this.direccion = direccion;
+        this.esPredeterminado = esPredeterminado;
     }
 
     @Override
--- donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/donantes/Donante.java
+++ donaciones-service/src/main/java/ar/edu/utn/frba/ddsi/donaciones/models/entities/donantes/Donante.java
@@ -1,24 +1,65 @@
 package ar.edu.utn.frba.ddsi.donaciones.models.entities.donantes;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.Direccion;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.Representante;
 import ar.edu.utn.frba.ddsi.donaciones.models.entities.contacto.MedioDeContacto;
+import ar.edu.utn.frba.ddsi.donaciones.models.entities.contacto.TipoMedioContacto;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public abstract class Donante {
-    protected String nombre;
+public class Donante {
+    private String nombre_razonSocial;
+    private String documento;
+    private TipoDocumento tipoDocumento;
+    private TipoPersona tipoPersona;
+    private Date fechaDeNacimiento_inicioActividad;
     public List<MedioDeContacto> mediosDeContacto;
+    public List<Representante> representantes;
+    private Rubro rubro;
+    private Genero genero;
+    private Direccion direccion;
 
-    public Donante(String nombre, List<MedioDeContacto> mediosDeContacto) {
+
+    /********** CONSTRUCTOR **********/
+    public Donante(
+            String unNombre_razonSocial,
+            String unDocumento,
+            TipoDocumento unTipoDocumento,
+            TipoPersona unTipoPersona,
+            Date unaFechaDeNacimiento_inicioActividad,
+            List<MedioDeContacto> mediosDeContacto,
+            List<Representante> representantes,
+            Rubro unRubro,
+            Genero unGenero,
+            Direccion unaDireccion
+    ) {
         // LISTA DE MEDIOS DE CONTACTO VACÍA
         if (mediosDeContacto == null || mediosDeContacto.isEmpty()) {
             throw new IllegalArgumentException("¡El donante debe tener al menos un medio de contacto!");
         }
 
-        this.nombre = nombre;
+        if (unTipoPersona == TipoPersona.FISICA){
+            boolean tieneEmail = mediosDeContacto.stream()
+                    .anyMatch(m -> m.getTipoMedioContacto() == TipoMedioContacto.EMAIL);
+            if (!tieneEmail) {
+                throw new IllegalArgumentException("¡El donante debe tener al menos un email!");
+            }
+        }
+
+        nombre_razonSocial = unNombre_razonSocial;
+        documento = unDocumento;
+        tipoDocumento = unTipoDocumento;
+        tipoPersona = unTipoPersona;
+        fechaDeNacimiento_inicioActividad = unaFechaDeNacimiento_inicioActividad;
         this.mediosDeContacto = new ArrayList<>(mediosDeContacto);
+        this.representantes = new ArrayList<>(representantes);
+        rubro = unRubro;
+        genero = unGenero;
+        direccion = unaDireccion;
     }
 
-    // MODIFICACIÓN DE LA LISTA DE MEDIOS DE CONTACTO
+    /********** MODIFICACIÓN DE LA LISTA DE MEDIOS DE CONTACTO **********/
     public void agregarMedio(MedioDeContacto nuevoMedioDeContacto) {
         if (nuevoMedioDeContacto == null) {
             throw new IllegalArgumentException("¡El medio a agregar no puede ser NULL!");
@@ -39,4 +80,26 @@ public abstract class Donante {
 
         mediosDeContacto.remove(medioDeContactoEliminado);
     }
+
+    /********** REPRESENTANTES **********/
+    public void agregarRepresentante(Representante nuevoRepresentante) {
+        if (this.representantes.contains(nuevoRepresentante)) {
+            throw new IllegalArgumentException("¡El representante ya está en la lista!");
+        }
+
+        this.representantes.add(nuevoRepresentante);
+    }
+    public void eliminarRepresentante(Representante representanteEliminado) {
+        if (!this.representantes.contains(representanteEliminado)) {
+            throw new IllegalArgumentException("¡El representante no está en la lista!");
+        }
+        if (this.representantes.size() == 1) {
+            // No me gusta que tire la misma excepción que arriba...
+            throw new IllegalArgumentException("¡El donante jurídico debe tener al menos un representante!");
+        }
+
+        this.representantes.remove(representanteEliminado);
+    }
+
+
 }
